116183c83d691ffe504277170cb5eccc0aaeab20
Ultramar <artyom.odinochenko@yandex.ru>
2022-04-05 16:04:59 +0300

    Lesson 2 task added

---
+++ src/Lesson2/Scan.java
@@ -0,0 +1,21 @@
+package Lesson2;
+
+import java.util.Scanner;
+
+public class Scan {
+
+    public static void main(String[] args) {
+        System.out.println("Enter your username:");
+        Scanner scanner = new Scanner(System.in);
+        String username = scanner.nextLine();
+        String spaces = "";
+        for (int i = 0; i < 19 - username.length(); i++) {
+            spaces += " ";
+        }
+        System.out.println("Hello, " + username + "!");
+        System.out.println("What is your credit card number, " + username + "?");
+        String cardNumber = scanner.nextLine();
+        System.out.println("------------------------\n|                      |\n|   " + cardNumber + "   |\n|                      |\n|   " + username + spaces + "|\n|                      |\n------------------------");
+    }
+
+}
